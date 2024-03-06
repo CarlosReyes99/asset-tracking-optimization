73a2903ec82a9d9b33d34235f5840bcb62d542e6
Carlos Reyes <carlosmxm055@gmail.com>
2024-03-06 12:57:58 -0600

    Style: Modifications to principal view

---
+++ src/main/java/Pdf/EmployeeDataPdf.java
@@ -0,0 +1,25 @@
+package pdf;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class EmployeeDataPdf {
+
+    String correo;
+    String nombreEmpleado;
+    String numeroEmpleado;
+    String departamento;
+    String descEquipo;
+    String modelEquipo;
+    String activoEquipo;
+    String numSerie;
+    String etiquetaEquipo;
+
+
+}
