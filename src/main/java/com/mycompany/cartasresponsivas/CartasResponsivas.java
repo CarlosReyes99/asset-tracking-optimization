51979d59056bd71663319694947c73d2c09532ff
Carlos Reyes <carlosmxm055@gmail.com>
2024-03-06 08:42:13 -0600

    Style: Principal view Ready

---
+++ src/main/java/com/mycompany/cartasresponsivas/CartasResponsivas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cartasresponsivas;
+
+/**
+ *
+ * @author desan
+ */
+public class CartasResponsivas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
